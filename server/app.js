171e4f2d2a6b2dadc2b8653a4ae3c18f031081d6
Dmitry <zadima@gmail.com>
2019-10-11 23:42:01 +0300

    views handling

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/build/:buildId', (req, res) => {
+  const { buildId } = req.params
+
+  res.render('build', { buildId })
+})
+
+app.listen(3000, 'localhost', () => {
+  console.log(`CI Server running on http://localhost:3000/`);
+})
