5f2f9510ef6843e30425f82a84c11e38d2847e50
Dmitry <zadima@gmail.com>
2019-10-13 00:47:21 +0300

    add agent service for server

---
+++ server/services/agent.js
@@ -0,0 +1,44 @@
+const uuid = require('uuid/v4')
+
+class Agent {
+  #agents = []
+
+  register = (address) => {
+    if (!address) throw new Error('Agent must have an address')
+
+    this.#agents.push({ address, builds: new Map() })
+  }
+
+  startBuild = () => {
+    const startTime = Date.now()
+    const buildId = uuid()
+
+    const agent = this.#getLeastLoadedAgent()
+
+    agent.builds.set(buildId, startTime)
+
+    return { id: buildId, startTime }
+  }
+
+  stopBuild = buildId => {
+    const stopTime = Date.now()
+
+    const agent = this.#agents.find(agent => agent.builds.has(buildId))
+
+    if (!agent) throw new Error('Build not found')
+
+    agent.builds.delete(buildId)
+
+    return { stopTime }
+  }
+
+  #getLeastLoadedAgent = () => {
+    const [minAgent, ...agents] = this.#agents
+
+    return agents.reduce((min, curr) => {
+      return curr.builds.size < min.builds.size ? curr : min
+    }, minAgent)
+  }
+}
+
+module.exports = new Agent()
